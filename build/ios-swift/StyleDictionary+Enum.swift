3e4cf794f3a8cd9e2f20a4d7082dbe2ee8cfa21f
Harm-Jan Abbing <h.abbing@vgz.nl>
2022-12-15 14:21:21 +0100

    Update tokens

--- build/ios-swift/StyleDictionary+Enum.swift
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -4,15 +4,36 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 15 Dec 2022 13:10:25 GMT
+// Generated on Thu, 15 Dec 2022 13:20:50 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryEnum {
-    public static let izzPrimairyBase = #951B81
-    public static let izzPrimairyDark = #4F1966
-    public static let izzPrimairyLight = #A74C97
+    public static let borderRadiusBase = 20
+    public static let borderRadiusCircle = 50%
+    public static let borderRadiusPill = 9999
+    public static let borderRadiusTiny = 4
+    public static let borderRasdiusSmall = 10
+    public static let colorCta = #006FCE
+    public static let colorPrimary = #0B6B6F
+    public static let colorSecondary = #0a9f8e
+    public static let colorTertiary = #22c4b4
+    public static let fontFamiltyText = Roboto
+    public static let fontFamilyTitle = Asap
+    public static let fontWeightBold = Bold
+    public static let fontWeightMedium = SemiBold
+    public static let fontWeightRegular = Regular
+    public static let h1 = [object Object]
+    public static let opacityFull = 1
+    public static let opacityMedium = 0.5
+    public static let opacityNone = 0
+    public static let opacityStrong = 0.75
+    public static let opacitySuble = 0.25
+    public static let primairyBase = #951B81
+    public static let primairyLight = #A74C97
+    public static let tokenSetOrder0 = VGZ
+    public static let tokenSetOrder1 = IZZ
     public static let vgzBorderRadiusBase = 20
     public static let vgzBorderRadiusCircle = 50%
     public static let vgzBorderRadiusPill = 9999
@@ -24,10 +45,22 @@ public enum StyleDictionaryEnum {
     public static let vgzColorTertiary = #22c4b4
     public static let vgzFontFamiltyText = Roboto
     public static let vgzFontFamilyTitle = Asap
+    public static let vgzFontSizeBase = 16
+    public static let vgzFontSizeLarge = 20
+    public static let vgzFontSizeMedium = 15
+    public static let vgzFontSizeSmall = 14
+    public static let vgzFontSizeTiny = 12
+    public static let vgzFontSizeXl = 26
+    public static let vgzFontSizeXxl = 32
     public static let vgzFontWeightBold = Bold
     public static let vgzFontWeightMedium = SemiBold
     public static let vgzFontWeightRegular = Regular
-    public static let vgzH1 = [object Object]
+    public static let vgzLineHeightBase = 150%
+    public static let vgzLineHeightLarge = 160%
+    public static let vgzLineHeightMedium = 140%
+    public static let vgzLineHeightSmall = 120%
+    public static let vgzLineHeightTiny = 100%
+    public static let vgzLineHeightXl = 200%
     public static let vgzOpacityFull = 1
     public static let vgzOpacityMedium = 0.5
     public static let vgzOpacityNone = 0
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,15 +4,36 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 15 Dec 2022 13:10:25 GMT
+// Generated on Thu, 15 Dec 2022 13:20:50 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
-    internal static let izzPrimairyBase = #951B81
-    internal static let izzPrimairyDark = #4F1966
-    internal static let izzPrimairyLight = #A74C97
+    internal static let borderRadiusBase = 20
+    internal static let borderRadiusCircle = 50%
+    internal static let borderRadiusPill = 9999
+    internal static let borderRadiusTiny = 4
+    internal static let borderRasdiusSmall = 10
+    internal static let colorCta = #006FCE
+    internal static let colorPrimary = #0B6B6F
+    internal static let colorSecondary = #0a9f8e
+    internal static let colorTertiary = #22c4b4
+    internal static let fontFamiltyText = Roboto
+    internal static let fontFamilyTitle = Asap
+    internal static let fontWeightBold = Bold
+    internal static let fontWeightMedium = SemiBold
+    internal static let fontWeightRegular = Regular
+    internal static let h1 = [object Object]
+    internal static let opacityFull = 1
+    internal static let opacityMedium = 0.5
+    internal static let opacityNone = 0
+    internal static let opacityStrong = 0.75
+    internal static let opacitySuble = 0.25
+    internal static let primairyBase = #951B81
+    internal static let primairyLight = #A74C97
+    internal static let tokenSetOrder0 = VGZ
+    internal static let tokenSetOrder1 = IZZ
     internal static let vgzBorderRadiusBase = 20
     internal static let vgzBorderRadiusCircle = 50%
     internal static let vgzBorderRadiusPill = 9999
@@ -24,10 +45,22 @@ internal struct StyleDictionaryStruct {
     internal static let vgzColorTertiary = #22c4b4
     internal static let vgzFontFamiltyText = Roboto
     internal static let vgzFontFamilyTitle = Asap
+    internal static let vgzFontSizeBase = 16
+    internal static let vgzFontSizeLarge = 20
+    internal static let vgzFontSizeMedium = 15
+    internal static let vgzFontSizeSmall = 14
+    internal static let vgzFontSizeTiny = 12
+    internal static let vgzFontSizeXl = 26
+    internal static let vgzFontSizeXxl = 32
     internal static let vgzFontWeightBold = Bold
     internal static let vgzFontWeightMedium = SemiBold
     internal static let vgzFontWeightRegular = Regular
-    internal static let vgzH1 = [object Object]
+    internal static let vgzLineHeightBase = 150%
+    internal static let vgzLineHeightLarge = 160%
+    internal static let vgzLineHeightMedium = 140%
+    internal static let vgzLineHeightSmall = 120%
+    internal static let vgzLineHeightTiny = 100%
+    internal static let vgzLineHeightXl = 200%
     internal static let vgzOpacityFull = 1
     internal static let vgzOpacityMedium = 0.5
     internal static let vgzOpacityNone = 0
