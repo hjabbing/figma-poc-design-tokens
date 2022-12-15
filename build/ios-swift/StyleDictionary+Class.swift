2ea24b0dfcfe829eb8bda827983dddfbd1d5245f
Harm-Jan Abbing <h.abbing@vgz.nl>
2022-12-15 13:54:45 +0100

    Update gitignore

--- build/ios-swift/StyleDictionary+Class.swift
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -4,11 +4,18 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 15 Dec 2022 10:53:14 GMT
+// Generated on Thu, 15 Dec 2022 12:54:16 GMT
 
 
 import UIKit
 
 public class StyleDictionaryClass {
-    public static let globalColorPrimary = #ab1cbd
+    public static let izzPrimairyBase = #951B81
+    public static let izzPrimairyDark = #4F1966
+    public static let izzPrimairyLight = #A74C97
+    public static let vgzCta = #006FCE
+    public static let vgzH1 = [object Object]
+    public static let vgzPrimairyBase = #0a9f8e
+    public static let vgzPrimairyDark = #0B6B6F
+    public static let vgzPrimairyLight = #22c4b4
 }
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,11 +4,18 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 15 Dec 2022 10:53:14 GMT
+// Generated on Thu, 15 Dec 2022 12:54:16 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
-    internal static let globalColorPrimary = #ab1cbd
+    internal static let izzPrimairyBase = #951B81
+    internal static let izzPrimairyDark = #4F1966
+    internal static let izzPrimairyLight = #A74C97
+    internal static let vgzCta = #006FCE
+    internal static let vgzH1 = [object Object]
+    internal static let vgzPrimairyBase = #0a9f8e
+    internal static let vgzPrimairyDark = #0B6B6F
+    internal static let vgzPrimairyLight = #22c4b4
 }
