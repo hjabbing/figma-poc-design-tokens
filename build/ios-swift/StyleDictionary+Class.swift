6ea787e05af523ad6d84b6884e78dc5ebb8658c1
Harm-Jan Abbing <h.abbing@vgz.nl>
2022-12-15 14:32:51 +0100

    Seperate tokens output

--- build/ios-swift/StyleDictionary+Class.swift
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -4,54 +4,59 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 15 Dec 2022 13:26:26 GMT
+// Generated on Thu, 15 Dec 2022 13:32:23 GMT
 
 
 import UIKit
 
 public class StyleDictionaryClass {
-    public static let borderRadiusBase = 20
-    public static let borderRadiusCircle = 50%
-    public static let borderRadiusPill = 9999
-    public static let borderRadiusSmall = 10
-    public static let borderRadiusTiny = 4
-    public static let colorCta = #006FCE
-    public static let colorPrimary = #0B6B6F
-    public static let colorSecondary = #0a9f8e
-    public static let colorTertiary = #22c4b4
-    public static let fontFamilyText = Roboto
-    public static let fontFamilyTitle = Asap
-    public static let fontSizeBase = 16
-    public static let fontSizeLarge = 20
-    public static let fontSizeMedium = 15
-    public static let fontSizeSmall = 14
-    public static let fontSizeTiny = 12
-    public static let fontSizeXl = 26
-    public static let fontSizeXxl = 32
-    public static let fontWeightBold = Bold
-    public static let fontWeightMedium = SemiBold
-    public static let fontWeightRegular = Regular
-    public static let lineHeightBase = 150%
-    public static let lineHeightLarge = 160%
-    public static let lineHeightMedium = 140%
-    public static let lineHeightSmall = 120%
-    public static let lineHeightTiny = 100%
-    public static let lineHeightXl = 200%
-    public static let opacityFull = 1
-    public static let opacityMedium = 0.5
-    public static let opacityNone = 0
-    public static let opacityStrong = 0.75
-    public static let opacitySuble = 0.25
-    public static let primairyBase = #951B81
-    public static let primairyDark = #4F1966
-    public static let primairyLight = #A74C97
-    public static let spacingBase = 24
-    public static let spacingLarge = 32
-    public static let spacingMedium = 16
-    public static let spacingSmall = 8
-    public static let spacingTiny = 4
-    public static let spacingXl = 48
-    public static let spacingXxl = 64
-    public static let tokenSetOrder0 = VGZ
-    public static let tokenSetOrder1 = IZZ
+    public static let izzColorCta = #006fce
+    public static let izzColorPrimary = #4F1966
+    public static let izzColorSecondary = #951B81
+    public static let izzColorTertiary = #A74C97
+    public static let vgzBorderRadiusBase = 20
+    public static let vgzBorderRadiusCircle = 50%
+    public static let vgzBorderRadiusPill = 9999
+    public static let vgzBorderRadiusTiny = 4
+    public static let vgzBorderRasdiusSmall = 10
+    public static let vgzBorderWidthBase = 2
+    public static let vgzBorderWidthLarge = 4
+    public static let vgzBorderWidthSmall = 1
+    public static let vgzColorCta = #006FCE
+    public static let vgzColorPrimary = #0B6B6F
+    public static let vgzColorSecondary = #0a9f8e
+    public static let vgzColorTertiary = #22c4b4
+    public static let vgzFontFamiltyText = Roboto
+    public static let vgzFontFamilyTitle = Asap
+    public static let vgzFontSizeBase = 16
+    public static let vgzFontSizeLarge = 20
+    public static let vgzFontSizeMedium = 15
+    public static let vgzFontSizeSmall = 14
+    public static let vgzFontSizeTiny = 12
+    public static let vgzFontSizeXl = 26
+    public static let vgzFontSizeXxl = 32
+    public static let vgzFontWeightBold = Bold
+    public static let vgzFontWeightMedium = SemiBold
+    public static let vgzFontWeightRegular = Regular
+    public static let vgzLineHeightBase = 150%
+    public static let vgzLineHeightLarge = 160%
+    public static let vgzLineHeightMedium = 140%
+    public static let vgzLineHeightSmall = 120%
+    public static let vgzLineHeightTiny = 100%
+    public static let vgzLineHeightXl = 200%
+    public static let vgzOpacityFull = 1
+    public static let vgzOpacityMedium = 0.5
+    public static let vgzOpacityNone = 0
+    public static let vgzOpacityStrong = 0.75
+    public static let vgzOpacitySuble = 0.25
+    public static let vgzSpacingBase = 24
+    public static let vgzSpacingLarge = 32
+    public static let vgzSpacingMedium = 16
+    public static let vgzSpacingSmall = 8
+    public static let vgzSpacingTiny = 4
+    public static let vgzSpacingXl = 48
+    public static let vgzSpacingXxl = 64
+    public static let vgzTextDecorationLine = line-through
+    public static let vgzTextDecorationNormal = none
+    public static let vgzTextDecorationUnderline = underline
 }
