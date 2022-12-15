635d257174f44e199afcab6e92997d54d4a8b315
Harm-Jan Abbing <h.abbing@vgz.nl>
2022-12-15 14:36:18 +0100

    Use tokens output

--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,59 +4,57 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 15 Dec 2022 13:32:23 GMT
+// Generated on Thu, 15 Dec 2022 13:34:39 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
-    internal static let izzColorCta = #006fce
-    internal static let izzColorPrimary = #4F1966
-    internal static let izzColorSecondary = #951B81
-    internal static let izzColorTertiary = #A74C97
-    internal static let vgzBorderRadiusBase = 20
-    internal static let vgzBorderRadiusCircle = 50%
-    internal static let vgzBorderRadiusPill = 9999
-    internal static let vgzBorderRadiusTiny = 4
-    internal static let vgzBorderRasdiusSmall = 10
-    internal static let vgzBorderWidthBase = 2
-    internal static let vgzBorderWidthLarge = 4
-    internal static let vgzBorderWidthSmall = 1
-    internal static let vgzColorCta = #006FCE
-    internal static let vgzColorPrimary = #0B6B6F
-    internal static let vgzColorSecondary = #0a9f8e
-    internal static let vgzColorTertiary = #22c4b4
-    internal static let vgzFontFamiltyText = Roboto
-    internal static let vgzFontFamilyTitle = Asap
-    internal static let vgzFontSizeBase = 16
-    internal static let vgzFontSizeLarge = 20
-    internal static let vgzFontSizeMedium = 15
-    internal static let vgzFontSizeSmall = 14
-    internal static let vgzFontSizeTiny = 12
-    internal static let vgzFontSizeXl = 26
-    internal static let vgzFontSizeXxl = 32
-    internal static let vgzFontWeightBold = Bold
-    internal static let vgzFontWeightMedium = SemiBold
-    internal static let vgzFontWeightRegular = Regular
-    internal static let vgzLineHeightBase = 150%
-    internal static let vgzLineHeightLarge = 160%
-    internal static let vgzLineHeightMedium = 140%
-    internal static let vgzLineHeightSmall = 120%
-    internal static let vgzLineHeightTiny = 100%
-    internal static let vgzLineHeightXl = 200%
-    internal static let vgzOpacityFull = 1
-    internal static let vgzOpacityMedium = 0.5
-    internal static let vgzOpacityNone = 0
-    internal static let vgzOpacityStrong = 0.75
-    internal static let vgzOpacitySuble = 0.25
-    internal static let vgzSpacingBase = 24
-    internal static let vgzSpacingLarge = 32
-    internal static let vgzSpacingMedium = 16
-    internal static let vgzSpacingSmall = 8
-    internal static let vgzSpacingTiny = 4
-    internal static let vgzSpacingXl = 48
-    internal static let vgzSpacingXxl = 64
-    internal static let vgzTextDecorationLine = line-through
-    internal static let vgzTextDecorationNormal = none
-    internal static let vgzTextDecorationUnderline = underline
+    internal static let borderRadiusBase = 20
+    internal static let borderRadiusCircle = 50%
+    internal static let borderRadiusPill = 9999
+    internal static let borderRadiusTiny = 4
+    internal static let borderRasdiusSmall = 10
+    internal static let borderWidthBase = 2
+    internal static let borderWidthLarge = 4
+    internal static let borderWidthSmall = 1
+    internal static let colorCta = #006fce
+    internal static let colorPrimary = #4F1966
+    internal static let colorSecondary = #951B81
+    internal static let colorTertiary = #A74C97
+    internal static let fontFamiltyText = Roboto
+    internal static let fontFamilyTitle = Asap
+    internal static let fontSizeBase = 16
+    internal static let fontSizeLarge = 20
+    internal static let fontSizeMedium = 15
+    internal static let fontSizeSmall = 14
+    internal static let fontSizeTiny = 12
+    internal static let fontSizeXl = 26
+    internal static let fontSizeXxl = 32
+    internal static let fontWeightBold = Bold
+    internal static let fontWeightMedium = SemiBold
+    internal static let fontWeightRegular = Regular
+    internal static let lineHeightBase = 150%
+    internal static let lineHeightLarge = 160%
+    internal static let lineHeightMedium = 140%
+    internal static let lineHeightSmall = 120%
+    internal static let lineHeightTiny = 100%
+    internal static let lineHeightXl = 200%
+    internal static let opacityFull = 1
+    internal static let opacityMedium = 0.5
+    internal static let opacityNone = 0
+    internal static let opacityStrong = 0.75
+    internal static let opacitySuble = 0.25
+    internal static let spacingBase = 24
+    internal static let spacingLarge = 32
+    internal static let spacingMedium = 16
+    internal static let spacingSmall = 8
+    internal static let spacingTiny = 4
+    internal static let spacingXl = 48
+    internal static let spacingXxl = 64
+    internal static let textDecorationLine = line-through
+    internal static let textDecorationNormal = none
+    internal static let textDecorationUnderline = underline
+    internal static let tokenSetOrder0 = VGZ
+    internal static let tokenSetOrder1 = IZZ
 }
