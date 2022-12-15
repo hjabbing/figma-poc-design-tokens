e76def7d4d009bff5983b4eb55f1694ad7f88f5c
Harm-Jan Abbing <h.abbing@vgz.nl>
2022-12-15 14:20:31 +0100

    Update styling

--- build/ios-swift/StyleDictionary+Class.swift
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 15 Dec 2022 12:57:52 GMT
+// Generated on Thu, 15 Dec 2022 13:10:25 GMT
 
 
 import UIKit
@@ -13,14 +13,24 @@ public class StyleDictionaryClass {
     public static let izzPrimairyBase = #951B81
     public static let izzPrimairyDark = #4F1966
     public static let izzPrimairyLight = #A74C97
-    public static let vgzCta = #006FCE
+    public static let vgzBorderRadiusBase = 20
+    public static let vgzBorderRadiusCircle = 50%
+    public static let vgzBorderRadiusPill = 9999
+    public static let vgzBorderRadiusTiny = 4
+    public static let vgzBorderRasdiusSmall = 10
+    public static let vgzColorCta = #006FCE
+    public static let vgzColorPrimary = #0B6B6F
+    public static let vgzColorSecondary = #0a9f8e
+    public static let vgzColorTertiary = #22c4b4
+    public static let vgzFontFamiltyText = Roboto
+    public static let vgzFontFamilyTitle = Asap
+    public static let vgzFontWeightBold = Bold
+    public static let vgzFontWeightMedium = SemiBold
+    public static let vgzFontWeightRegular = Regular
     public static let vgzH1 = [object Object]
     public static let vgzOpacityFull = 1
     public static let vgzOpacityMedium = 0.5
     public static let vgzOpacityNone = 0
     public static let vgzOpacityStrong = 0.75
     public static let vgzOpacitySuble = 0.25
-    public static let vgzPrimairyBase = #0a9f8e
-    public static let vgzPrimairyDark = #0B6B6F
-    public static let vgzPrimairyLight = #22c4b4
 }
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 15 Dec 2022 12:57:52 GMT
+// Generated on Thu, 15 Dec 2022 13:10:25 GMT
 
 
 import UIKit
@@ -13,14 +13,24 @@ internal struct StyleDictionaryStruct {
     internal static let izzPrimairyBase = #951B81
     internal static let izzPrimairyDark = #4F1966
     internal static let izzPrimairyLight = #A74C97
-    internal static let vgzCta = #006FCE
+    internal static let vgzBorderRadiusBase = 20
+    internal static let vgzBorderRadiusCircle = 50%
+    internal static let vgzBorderRadiusPill = 9999
+    internal static let vgzBorderRadiusTiny = 4
+    internal static let vgzBorderRasdiusSmall = 10
+    internal static let vgzColorCta = #006FCE
+    internal static let vgzColorPrimary = #0B6B6F
+    internal static let vgzColorSecondary = #0a9f8e
+    internal static let vgzColorTertiary = #22c4b4
+    internal static let vgzFontFamiltyText = Roboto
+    internal static let vgzFontFamilyTitle = Asap
+    internal static let vgzFontWeightBold = Bold
+    internal static let vgzFontWeightMedium = SemiBold
+    internal static let vgzFontWeightRegular = Regular
     internal static let vgzH1 = [object Object]
     internal static let vgzOpacityFull = 1
     internal static let vgzOpacityMedium = 0.5
     internal static let vgzOpacityNone = 0
     internal static let vgzOpacityStrong = 0.75
     internal static let vgzOpacitySuble = 0.25
-    internal static let vgzPrimairyBase = #0a9f8e
-    internal static let vgzPrimairyDark = #0B6B6F
-    internal static let vgzPrimairyLight = #22c4b4
 }
