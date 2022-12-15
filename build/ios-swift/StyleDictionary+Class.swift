ca9903198abe266c063f70ef84908f19e08330fd
Harm-Jan Abbing <h.abbing@vgz.nl>
2022-12-15 13:59:54 +0100

    Attempt to use pull hook

--- build/ios-swift/StyleDictionary+Class.swift
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 15 Dec 2022 12:54:16 GMT
+// Generated on Thu, 15 Dec 2022 12:57:52 GMT
 
 
 import UIKit
@@ -15,6 +15,11 @@ public class StyleDictionaryClass {
     public static let izzPrimairyLight = #A74C97
     public static let vgzCta = #006FCE
     public static let vgzH1 = [object Object]
+    public static let vgzOpacityFull = 1
+    public static let vgzOpacityMedium = 0.5
+    public static let vgzOpacityNone = 0
+    public static let vgzOpacityStrong = 0.75
+    public static let vgzOpacitySuble = 0.25
     public static let vgzPrimairyBase = #0a9f8e
     public static let vgzPrimairyDark = #0B6B6F
     public static let vgzPrimairyLight = #22c4b4
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 15 Dec 2022 12:54:16 GMT
+// Generated on Thu, 15 Dec 2022 12:57:52 GMT
 
 
 import UIKit
@@ -15,6 +15,11 @@ internal struct StyleDictionaryStruct {
     internal static let izzPrimairyLight = #A74C97
     internal static let vgzCta = #006FCE
     internal static let vgzH1 = [object Object]
+    internal static let vgzOpacityFull = 1
+    internal static let vgzOpacityMedium = 0.5
+    internal static let vgzOpacityNone = 0
+    internal static let vgzOpacityStrong = 0.75
+    internal static let vgzOpacitySuble = 0.25
     internal static let vgzPrimairyBase = #0a9f8e
     internal static let vgzPrimairyDark = #0B6B6F
     internal static let vgzPrimairyLight = #22c4b4
